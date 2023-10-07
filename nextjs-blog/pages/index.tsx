5daab6b24d6e596583b40c92422ee2b3112668b7
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2023-10-07 17:22:42 +0200

    feat: convert blog from js to ts

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -1,3 +1,4 @@
+import {GetStaticProps} from 'next';
 import Link from 'next/link';
 import Head from 'next/head';
 import {getSortedPostsData} from '../lib/posts';
@@ -5,7 +6,15 @@ import Layout, {siteTitle} from '../components/layout';
 import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home({allPostsData}) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -37,7 +46,7 @@ export default function Home({allPostsData}) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
